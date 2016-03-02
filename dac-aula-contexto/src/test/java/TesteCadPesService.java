d8181daa82f8f2acb36cb373d6bba0f04f81de86
Nanda <fernandaalvesmonteiro7@gmail.com>
2016-03-02 10:58:45 -0300

    Teste listar Pronto

--- dac-aula-contexto/src/test/java/TesteCadPesService.java
+++ dac-aula-contexto/src/test/java/TesteCadPesService.java
@@ -4,9 +4,12 @@
  * and open the template in the editor.
  */
 
+import com.sun.glass.events.WheelEvent;
 import ifpb.dac.contexto.pessoa.CadastroPessoaService;
 import ifpb.dac.contexto.pessoa.Pessoa;
 import ifpb.dac.contexto.pessoa.ReposistorioDePessoa;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,6 +17,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
 
 /**
  *
@@ -30,7 +34,7 @@ public class TesteCadPesService {
     @Test
     public void testSalvar(){
         
-        reposistorioDePessoa = Mockito.mock(ReposistorioDePessoa.class);
+        reposistorioDePessoa = mock(ReposistorioDePessoa.class);
         cadastroPessoaService = new CadastroPessoaService(reposistorioDePessoa);
         
         Pessoa p = new Pessoa("Fernanda");
@@ -41,5 +45,19 @@ public class TesteCadPesService {
         
     }
     
+    @Test
+    public void testListar(){
+        
+        reposistorioDePessoa = mock(ReposistorioDePessoa.class);
+        
+        List<Pessoa> pessoas = new ArrayList<Pessoa>();
+        Pessoa pessoa = new Pessoa("Rodolfo");
+        pessoas.add(pessoa);
+        
+        Mockito.when(reposistorioDePessoa.todos()).thenReturn(pessoas);
+        
+        assertNotNull(reposistorioDePessoa.todos());
+        
+    }
 
 }
